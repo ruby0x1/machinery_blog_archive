7e53aa5366f85a5a1c5c6075e609aea8c9e1316b
ruby0x1 <ruby0x1@pm.me>
2022-08-01 18:52:39 -0700

    initial commit of publicly available content

--- README.md
+++ README.md
@@ -1,2 +1,11 @@
-# machinery_blog_archive
-A mirror of the publicly available Our Machinery Blog Posts from archive.org. Archived for posterity as a public service. Removed by request if it's a problem.
+# machinery blog archive
+A mirror of the **publicly available** Our Machinery Blog Posts from archive.org. 
+https://web.archive.org/https://ourmachinery.com/
+
+Archived for posterity as a public service. Removed by request if it's a problem.
+The pages have been stripped back to just the blog content as much as possible.
+There may be missing images in the archive.
+
+The blog was an invaluable resource to many and the strange circumstances of it's disappearance are a disservice to the work they've put into their engine and blog. It felt pertinent to make sure some or all of it was archived.
+If for some reason the publicly available blog posts are no longer allowed to be stored publicly we'll oblige by removing them from here. 
+
---
+++ unpkg.com/ionicons@5.4.0/dist/ionicons/ionicons.esm.js
@@ -0,0 +1,36 @@
+var _____WB$wombat$assign$function_____ = function(name) {return (self._wb_wombat && self._wb_wombat.local_init && self._wb_wombat.local_init(name)) || self[name]; };
+if (!self.__WB_pmw) { self.__WB_pmw = function(obj) { this.__WB_source = obj; return this; } }
+{
+  let window = _____WB$wombat$assign$function_____("window");
+  let self = _____WB$wombat$assign$function_____("self");
+  let document = _____WB$wombat$assign$function_____("document");
+  let location = _____WB$wombat$assign$function_____("location");
+  let top = _____WB$wombat$assign$function_____("top");
+  let parent = _____WB$wombat$assign$function_____("parent");
+  let frames = _____WB$wombat$assign$function_____("frames");
+  let opener = _____WB$wombat$assign$function_____("opener");
+
+import{b as i}from"./p-6f4eae92.js";import{p as a}from"./p-4372c4bc.js";a().then(a=>i([["p-206e6cb0",[[1,"ion-icon",{mode:[1025],color:[1],ariaLabel:[1537,"aria-label"],ariaHidden:[513,"aria-hidden"],ios:[1],md:[1],flipRtl:[4,"flip-rtl"],name:[1],src:[1],icon:[8],size:[1],lazy:[4],sanitize:[4],svgContent:[32],isVisible:[32]}]]]],a));
+
+}
+/*
+     FILE ARCHIVED ON 22:58:47 May 29, 2022 AND RETRIEVED FROM THE
+     INTERNET ARCHIVE ON 00:54:11 Aug 01, 2022.
+     JAVASCRIPT APPENDED BY WAYBACK MACHINE, COPYRIGHT INTERNET ARCHIVE.
+
+     ALL OTHER CONTENT MAY ALSO BE PROTECTED BY COPYRIGHT (17 U.S.C.
+     SECTION 108(a)(3)).
+*/
+/*
+playback timings (ms):
+  captures_list: 655.752
+  exclusion.robots: 0.089
+  exclusion.robots.policy: 0.081
+  RedisCDXSource: 396.514
+  esindex: 0.011
+  LoadShardBlock: 217.346 (3)
+  PetaboxLoader3.datanode: 86.705 (4)
+  CDXLines.iter: 22.343 (3)
+  load_resource: 162.675
+  PetaboxLoader3.resolve: 118.095
+*/
